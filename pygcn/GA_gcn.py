b91ba132bcd6d2aa14a451841881a8294a313af9
duydn95 <d222nguy@uwaterloo.ca>
2020-04-21 00:59:34 -0400

    night

--- pygcn/GA_gcn.py
+++ pygcn/GA_gcn.py
@@ -9,103 +9,44 @@ import torch.optim as optim
 from numpy.random import choice
 import numpy as np
 import matplotlib.pyplot as plt
-def generate_population(size, x_boundaries, y_boundaries):
-    lower_x, upper_x = x_boundaries
-    lower_y, upper_y = y_boundaries
-    population = []
-    for i in range(size):
-        individual = {
-            "x": random.uniform(lower_x, upper_x),
-            "y": random.uniform(lower_y, upper_y)
-        } 
-        population.append(individual)
-    return population
 
-def apply_function(individual):
-    x = individual["x"]
-    y = individual["y"]
-    return math.sin(math.sqrt(x ** 2 + y ** 2))
-def sorted_population_by_fitness(population):
-    return sorted(population, key = apply_function)
-def crossover(individual_a, individual_b):
-    xa = individual_a["x"]
-    ya = individual_a["y"]
+global nfeat
+global nclass
 
-    xb = individual_b["x"]
-    yb = individual_b["y"]
 
-    return {"x": (xa + xb)/2, "y": (ya + yb)/2}
-def mutate(individual):
-    next_x = individual["x"] + random.uniform(-0.05, 0.05)
-    next_y = individual["y"] + random.uniform(-0.05, 0.05)
-    lower_boundary, upper_boundary = (-4, 4)
-    next_x = min(max(next_x, lower_boundary), upper_boundary)
-    next_y = min(max(next_y, lower_boundary), upper_boundary)
 
-    return {"x": next_x, "y": next_y}
-def choice_by_roulette(sorted_population, fitness_sum):
-    offset = 0
-    normalized_fitness_sum = fitness_sum
-    lowest_fitness = apply_function(sorted_population[0])
-    if lowest_fitness < 0:
-        offset = -lowest_fitness
-        normalized_fitness_sum += offset * len(sorted_population)
-    draw = random.uniform(0, 1)
-    accumulated = 0
-    for individual in sorted_population:
-        fitness = apply_function(individual) + offset
-        probability = fitness / normalized_fitness_sum
-        accumulated += probability
-
-        if draw <= accumulated:
-            return individual
-def make_next_generation(previous_population):
-    next_generation = []
-    sorted_by_fitness_population = sorted_population_by_fitness(previous_population)
-    population_size = len(previous_population)
-    fitness_sum = sum(apply_function(individual) for individual in previous_population)
-
-    for i in range(population_size):
-        first_choice = choice_by_roulette(sorted_by_fitness_population, fitness_sum)
-        second_choice = choice_by_roulette(sorted_by_fitness_population, fitness_sum)
-
-        individual = crossover(first_choice, second_choice)
-        individual = mutate(individual)
-        next_generation.append(individual)
-    return next_generation
-def set_params_range(params_range):
-    '''Set params range for all kinds of hyperparameters in the network'''
-    params_range["n_hidden"]["lower"] = 5
-    params_range["n_hidden"]["upper"] = 50
-    params_range["dropout"]["lower"] = 0.1
-    params_range["dropout"]["upper"] = 0.8
-    params_range["learning_rate"]["lower"] = -6 #this param is searched on log scale
-    params_range["learning_rate"]["upper"] = 1
-    lower_x, upper_x = x_boundaries
-    lower_y, upper_y = y_boundaries
-    population = []
-    # for i in range(size):
-    #     individual = {
-    #         "x": random.uniform(lower_x, upper_x),
-    #         "y": random.uniform(lower_y, upper_y)
-    #     } 
-    #     population.append(individual)
-    return population
 def gcn_generate_individual():
+    global nfeat
+    global nclass
     x = {}
     x = {"seed": 42,
-        "nfeat": 1433,
-        "nclass": 7,
+        "nfeat": nfeat,
+        "nclass": nclass,
+        "epochs": 40,
+        "n_hidden": random.randint(5, 100),
+        "dropout": random.uniform(0.0, 0.8),
+        "lr": random.uniform(-5, 0),
+        "weight_decay": random.uniform(-5, 0)
+        }
+    return x
+def gcn_sample_individual():
+    global nfeat
+    global nclass
+    x = {}
+    x = {"seed": 42,
+        "nfeat": nfeat,
+        "nclass": nclass,
         "epochs": 10,
-        "n_hidden": random.randint(5, 80),
-        "dropout": random.uniform(0.1, 0.8),
-        "lr": random.uniform(-6, 1),
-        "weight_decay": random.uniform(-6, 1)
+        "n_hidden": 16,
+        "dropout": 0.5,
+        "lr": np.log10(0.01),
+        "weight_decay": -4 + np.log10(5)
         }
     return x
 def gcn_generate_population(size):
     population = []
-    for i in range(size):
+    population.append(gcn_sample_individual())
+    for i in range(1, size):
         population.append(gcn_generate_individual())
     return population
 def gcn_crossover(A, B):
@@ -126,34 +67,32 @@ def a_random(param):
     elif param == "dropout":
         return random.uniform(0.1, 0.8)
     elif param == "lr":
-        return random.uniform(-6, 1)
+        return random.uniform(-4, 0)
     elif param == "weight_decay":
         return random.uniform(-6, 0)
+def winsorize(indi):
+    indi["n_hidden"] = int(min(100, max(indi["n_hidden"], 5)))
+    indi["dropout"] = min(1, max(indi["dropout"], 0))
+    return indi
 def gcn_mutation(A):
     '''Perform mutation on individual A'''
     param_list = ["n_hidden", "dropout", "weight_decay","lr"]
     for param in param_list:
         p = random.uniform(0, 1) #toss a coin, whether to mutate this param
         if p < 0.05:
-            A[param] = a_random(param)
-    return A
-
-def main():
-    generations = 100
-    population = generate_population(size = 10, x_boundaries = (-4, 4), y_boundaries = (-4, 4))
-    i = 1
-    while True:
-        print(f" Generation {i}")
-        for individual in population:
-            print(individual)
-        if i == generations:
-            break
-        i += 1
-        population = make_next_generation(population)
-    best_individual = sorted_population_by_fitness(population)[-1]
-    print("\n FINAL RESULT")
-    print(best_individual, apply_function(best_individual))
-
+            #phi = random.uniform(-1, 1)
+            A[param] = (A[param] + a_random(param))/2.0
+    return winsorize(A)
+def get_fitness(network, n_tries):
+    s = 0
+    #Adaptive n_tries
+    for i in range(n_tries):
+        p = train(network)
+        if (p < 0.5):
+            return p
+        s += train(network)
+    s /= n_tries
+    return s
 def train(network):
     t_total = time.time()
     model = network.model
@@ -188,7 +127,7 @@ def choice_by_fitness(sorted_population, prob):
 def fitness(indi):
     #return indi["acc_val"]/(1 - indi["acc_val"])
     return indi["acc_val"]
-def gcn_make_next_generation(sorted_population):
+def gcn_make_next_generation(sorted_population, size):
     fitness_sum = 0
     p = [-1 for i in range(len(sorted_population))]
     new_population = []
@@ -201,7 +140,6 @@ def gcn_make_next_generation(sorted_population):
     #When fitnesses range are close, top individuals should be rewarded more
 
     #print(p)
-    size = 30
     for i in range(size):
         father = choice_by_fitness(sorted_population, p)
         mother = choice_by_fitness(sorted_population, p)
@@ -211,18 +149,19 @@ def gcn_make_next_generation(sorted_population):
     return new_population
 def test(network):
     model = network.model
+
     model.eval()
     output = model(features, adj)
     loss_test = F.nll_loss(output[idx_test], labels[idx_test])
     acc_test = accuracy(output[idx_test], labels[idx_test])
     print("Test set results:",
-          "loss= {:.4f}".format(loss_test.item()),
-          "accuracy= {:.4f}".format(acc_test.item()))
+        "loss= {:.4f}".format(loss_test.item()),
+        "accuracy= {:.4f}".format(acc_test.item()))
     return acc_test.item()
 def set_params(params):
     params["epochs"] = 200
-    params["lr"] = 0.01
-    params["weight_decay"] = 5e-4
+    params["lr"] = -2
+    params["weight_decay"] = -4
     params["n_hidden"] = 16
     params["dropout"] = 0.2
     params["seed"] = 42
@@ -239,11 +178,12 @@ def short_print(indi):
         indi["n_hidden"], indi["dropout"], indi["weight_decay"], indi["lr"], indi["acc_val"]
     ))
 def simulate():
-    simulations = 10
+    simulations = 1
     generations = 10
     values = np.zeros(generations)
     size = 15
     itr = range(generations)
+    best = 0
     for s in range(simulations):
         print("===================Simulation- {0}===================".format(s))
         population = gcn_generate_population(size)
@@ -252,22 +192,35 @@ def simulate():
             print("--Generation ", i)
             for indi in population:
                 net = NetworkInstance(**indi)
-                indi["acc_val"] = train(net)
+                indi["acc_val"] = get_fitness(net, 2)
             population = sorted_by_fitness_population(population)
             # for indi in population:
             #     short_print(indi)
             print_statistics(population)
             optimality_tracking.append(population[-1]["acc_val"])
+            if population[-1]["acc_val"] > best:
+                best = population[-1]["acc_val"]
+                optimal_sol = population[-1]
             if (i != generations - 1):
-                population = gcn_make_next_generation(population)
+                population = gcn_make_next_generation(population, size)
         values += np.array(optimality_tracking)
+    optimal_sol["epochs"] = 200
+    print('best = ', best)
+    print('optimal_sol = ', optimal_sol)
+    s = 0
+    for i in range(100):
+        net = NetworkInstance(**optimal_sol)
+        train(net)
+        s += test(net)
+    s /= 100
+    print(s)
     values /= simulations
     plt.plot(itr, values, lw = 0.5)
     plt.show()
     print(values)
 
 if __name__ == '__main__':
-    adj, features, labels, idx_train, idx_val, idx_test = load_data()
+    adj, features, labels, idx_train, idx_val, idx_test = load_data("pubmed")
 
     features = features.cuda()
     adj = adj.cuda()
@@ -279,14 +232,30 @@ if __name__ == '__main__':
     params = {}
     params["nfeat"] = features.shape[1]
     params["nclass"] = labels.max().item() + 1
+    nfeat = params["nfeat"]
+    nclass = params["nclass"]
     #params["nclass"] = 7
-    set_params(params)
+    #set_params(params)
     #pop = gcn_generate_population(10)
     #print(pop[0], pop[8])
     #a = gcn_crossover(pop[0], pop[8])
     #print(a)
     #print(gcn_mutation(a))
     simulate()
+    # res = 0
+    # for i in range(20):
+    #     params["epochs"] = 200
+    #     params["n_hidden"] = 16
+    #     params["dropout"] = 0.5
+    #     params["weight_decay"] = -4 + np.log10(5)
+    #     params["lr"] = np.log10(0.01)
+    #     params["seed"] = 42
+    #     net = NetworkInstance(**params)
+    #     train(net)
+    #     res += test(net)
+    # res /= 20
+    # print(res)
+    
 
     # generations = 10
     # size = 30
--- pygcn/train.py
+++ pygcn/train.py
@@ -9,7 +9,6 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
-from pygcn.utils import load_data, accuracy
 from pygcn.models import GCN
 class NetworkInstance:
     def __init__(self, **kwargs):
@@ -24,10 +23,6 @@ class NetworkInstance:
         np.random.seed(self.params["seed"])
         torch.cuda.manual_seed(self.params["seed"])
 
-        # Load data
-        # self.adj, self.features, self.labels, self.idx_train, self.idx_val, self.idx_test = load_data()
-        # adjT = torch.transpose(adj, 0, 1)
-
         # Model and optimizer
         self.model = GCN(nfeat=self.params["nfeat"],
                     nhid=self.params["n_hidden"],
@@ -36,49 +31,3 @@ class NetworkInstance:
         self.optimizer = optim.Adam(self.model.parameters(),
                             lr=10 ** self.params["lr"], weight_decay=10 ** self.params["weight_decay"])
         self.model.cuda()
-
-#     def train(self, epoch):
-#         t = time.time()
-#         self.model.train()
-#         self.optimizer.zero_grad()
-#         output = self.model(features, adj)
-#         loss_train = F.nll_loss(output[idx_train], labels[idx_train])
-#         acc_train = accuracy(output[idx_train], labels[idx_train])
-#         loss_train.backward()
-#         optimizer.step()
-
-#         if not args.fastmode:
-#             # Evaluate validation set performance separately,
-#             # deactivates dropout during validation run.
-#             model.eval()
-#             output = model(features, adj)
-
-#         loss_val = F.nll_loss(output[idx_val], labels[idx_val])
-#         acc_val = accuracy(output[idx_val], labels[idx_val])
-#         print('Epoch: {:04d}'.format(epoch+1),
-#             'loss_train: {:.4f}'.format(loss_train.item()),
-#             'acc_train: {:.4f}'.format(acc_train.item()),
-#             'loss_val: {:.4f}'.format(loss_val.item()),
-#             'acc_val: {:.4f}'.format(acc_val.item()),
-#             'time: {:.4f}s'.format(time.time() - t))
-
-
-# def test():
-#     model.eval()
-#     output = model(features, adj)
-#     loss_test = F.nll_loss(output[idx_test], labels[idx_test])
-#     acc_test = accuracy(output[idx_test], labels[idx_test])
-#     print("Test set results:",
-#           "loss= {:.4f}".format(loss_test.item()),
-#           "accuracy= {:.4f}".format(acc_test.item()))
-
-def do():
-    # Train model
-    t_total = time.time()
-    for epoch in range(args.epochs):
-        train(epoch)
-    print("Optimization Finished!")
-    print("Total time elapsed: {:.4f}s".format(time.time() - t_total))
-
-    # Testing
-    test()
